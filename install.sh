3e806387bfab10d3aea8ff7964e94debe22734e6
sloan58 <marty@karmatek.io>
2021-05-26 12:45:47 -0400

    full install working

--- install.sh
+++ install.sh
@@ -9,36 +9,40 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "${blue}----------Starting Installation----------" > $(tty)
 
-echo "Running apt package updates" > $(tty)
-apt-get update && apt-get upgrade -y
-
 echo "Setting the Server Timezone to America/New_York" > $(tty)
 echo "America/New_York" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 
-echo "Enabling Ubuntu Firewall.  Allowing SSH, HTTP and HTTPS" > $(tty)
+echo "Enabling Ubuntu Firewall. Allowing SSH, HTTP and HTTPS" > $(tty)
 ufw enable
 ufw allow 22
 ufw allow 80
 ufw allow 443
 
+echo "Running system updates" > $(tty)
+apt-get update && apt-get upgrade -y
+
 echo "Installing system and application dependencies" > $(tty)
 apt-get install -y php7.4 php7.4-cli php7.4-common php7.4-json \
                    php7.4-opcache php7.4-mysql php7.4-mbstring \
-                   php7.4-zip php7.4-fpm php7.4-xml php7.4-ldap redis \
-                   mysql-server mysql-client nginx git curl nano
+                   php7.4-zip php7.4-fpm php7.4-xml php7.4-ldap \
+                   redis-server mysql-server mysql-client nginx \
+                   git curl nano
 
 
 echo "Starting php fpm" > $(tty)
 service php7.4-fpm start
 
+echo "Starting Redis server" > $(tty)
+service redis-server start
+
 echo "Installing the composer package manager" > $(tty)
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-export MYSQL_PWD=`date +%s | sha256sum | base64 | head -c 32 ; echo;`
-
 echo "Starting MySQL service" > $(tty)
 service mysql start
+
+export MYSQL_PWD=`date +%s | sha256sum | base64 | head -c 32 ; echo;`
 
 echo "Running MySQL secure installation" > $(tty)
 sudo mysql_secure_installation 2>/dev/null <<MSI
@@ -59,6 +63,9 @@ echo "Updating MySQL root authentication settings" > $(tty)
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PWD}';"
 mysql -u root -p${MYSQL_PWD} -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
 
+echo "Creating CMS Player database" > $(tty)
+mysql -uroot -p${MYSQL_PWD} -e 'CREATE DATABASE cmsplayer;'
+
 echo "Configuring nginx" > $(tty)
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
@@ -78,8 +85,22 @@ cd ${APP_DIR}
 composer install
 
 echo "Configuring app environment" > $(tty)
+export MOUNT_PATH=`which mount`
 cp .env.example .env
+sed -i 's/APP_NAME=Laravel/APP_NAME="CMS Player"/g' .env
+sed -i 's/APP_ENV=local/APP_ENV=production/g' .env
+sed -i 's/APP_DEBUG=true/APP_DEBUG=false/g' .env
+sed -i 's/DB_DATABASE=cmsrec/DB_DATABASE=cmsplayer/g' .env
+sed -i "s/DB_PASSWORD=/DB_PASSWORD=${MYSQL_PWD}/g" .env
+sed -i 's/CACHE_DRIVER=file/CACHE_DRIVER=redis/g' .env
+sed -i 's/QUEUE_CONNECTION=sync/QUEUE_CONNECTION=redis/g' .env
+sed -i 's/SESSION_DRIVER=file/SESSION_DRIVER=redis/g' .env
+sed -i "s|MOUNT_PATH=/bin|MOUNT_PATH=${MOUNT_PATH}|g" .env
 php artisan key:generate
+php artisan migrate --force --seed
+
+echo "Setting app file permissions" > $(tty)
+chown -R www-data: ${APP_DIR}
 
 echo $reset > $(tty)
 echo "Installation Complete! Please find your application/server details below:" > $(tty)
